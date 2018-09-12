886d78fad89f6831478277992526f8f6cf221a91
Benjamin <b.cherlonneix@gmail.com>
2018-09-12 16:51:44 +0200

    Ajout singe

--- app/src/main/java/fr/ben/jp/monpremierprojet/mainActivity.java
+++ app/src/main/java/fr/ben/jp/monpremierprojet/mainActivity.java
@@ -8,19 +8,40 @@ import android.widget.ImageView;
 
 public class mainActivity extends AppCompatActivity{
 
+    MediaPlayer mPlayer = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final MediaPlayer mPlayer = MediaPlayer.create(this, R.raw.bouc);
 
-        ImageView img = (ImageView)findViewById(R.id.imageChevre);
-        img.setOnClickListener(new View.OnClickListener() {
+
+        ImageView imgChevre = (ImageView)findViewById(R.id.imageChevre);
+
+        imgChevre.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mPlayer.start();
+                playSound(R.raw.bouc);
             }
         });
+
+        ImageView imgSinge = (ImageView)findViewById(R.id.imageSinge);
+
+        imgSinge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                playSound(R.raw.singe);
+            }
+        });
+    }
+
+    private void playSound(int resId) {
+        if(mPlayer != null) {
+            mPlayer.stop();
+            mPlayer.release();
+        }
+        mPlayer = MediaPlayer.create(this, resId);
+        mPlayer.start();
     }
 }
